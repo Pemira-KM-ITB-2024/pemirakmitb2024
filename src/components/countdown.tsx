99a508b79a4d13bc17d5c3377535e887517e1c85
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-10-19 20:19:19 +0700

    update countdown responsive

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,23 +1,33 @@
 import { header } from "@fonts";
 
+import React from "react";
+
 const Countdown = () => {
   return (
-    <div className={`flex justify-center gap-20 ${header.className}`}>
+    <div className={`flex justify-center gap-8 md:gap-20 ${header.className}`}>
       <div className="flex flex-col items-center">
-        <div className="text-9xl text-white">62</div>
-        <div className="text-xl text-white">HARI</div>
+        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">62</div>
+        <div className="text-[11px] text-white md:text-base lg:text-xl">
+          HARI
+        </div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="text-9xl text-white">13</div>
-        <div className="text-xl text-white">JAM</div>
+        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">13</div>
+        <div className="text-[11px] text-white md:text-base lg:text-xl">
+          JAM
+        </div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="text-9xl text-white">59</div>
-        <div className="text-xl text-white">MENIT</div>
+        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">59</div>
+        <div className="text-[11px] text-white md:text-base lg:text-xl">
+          MENIT
+        </div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="text-9xl text-white">48</div>
-        <div className="text-xl text-white">DETIK</div>
+        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">48</div>
+        <div className="text-[11px] text-white md:text-base lg:text-xl">
+          DETIK
+        </div>
       </div>
     </div>
   );
