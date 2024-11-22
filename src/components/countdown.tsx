4e0e1670a7639040d317a65e2f40154ca5135fa9
Fozzzzy <122066488+andrewtedja@users.noreply.github.com>
2024-11-22 23:11:14 +0700

    feat: update countdown with fetch time left

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,33 +1,52 @@
+import { useEffect, useState } from "react";
 import { header } from "@fonts";
 
-import React from "react";
-
 const Countdown = () => {
+  const electionDate = new Date("2024-12-25T00:00:00+07:00");
+
+  const [timeLeft, setTimeLeft] = useState(
+    Math.floor((electionDate.getTime() - new Date().getTime()) / 1000),
+  );
+
+  const getTimeLeft = {
+    days: Math.max(0, Math.floor(timeLeft / (60 * 60 * 24)))
+      .toString()
+      .padStart(2, "0"),
+    hours: Math.max(0, Math.floor((timeLeft % (60 * 60 * 24)) / (60 * 60)))
+      .toString()
+      .padStart(2, "0"),
+    minutes: Math.max(0, Math.floor((timeLeft % (60 * 60)) / 60))
+      .toString()
+      .padStart(2, "0"),
+    seconds: Math.max(0, timeLeft % 60)
+      .toString()
+      .padStart(2, "0"),
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => Math.max(0, prev - 1));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className={`flex justify-center gap-8 md:gap-20 ${header.className}`}>
+    <div className={`flex justify-center gap-20 ${header.className}`}>
       <div className="flex flex-col items-center">
-        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">62</div>
-        <div className="text-[11px] text-white md:text-base lg:text-xl">
-          HARI
-        </div>
+        <div className="text-9xl text-white">{getTimeLeft.days}</div>
+        <div className="text-xl text-white">HARI</div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">13</div>
-        <div className="text-[11px] text-white md:text-base lg:text-xl">
-          JAM
-        </div>
+        <div className="text-9xl text-white">{getTimeLeft.hours}</div>
+        <div className="text-xl text-white">JAM</div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">59</div>
-        <div className="text-[11px] text-white md:text-base lg:text-xl">
-          MENIT
-        </div>
+        <div className="text-9xl text-white">{getTimeLeft.minutes}</div>
+        <div className="text-xl text-white">MENIT</div>
       </div>
       <div className="flex flex-col items-center">
-        <div className="text-[44px] text-white md:text-6xl lg:text-9xl">48</div>
-        <div className="text-[11px] text-white md:text-base lg:text-xl">
-          DETIK
-        </div>
+        <div className="text-9xl text-white">{getTimeLeft.seconds}</div>
+        <div className="text-xl text-white">DETIK</div>
       </div>
     </div>
   );
