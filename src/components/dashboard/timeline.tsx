f91994b84895df34e8213eb371ef486535fbab61
Kenneth <kennethpoenadi26@gmail.com>
2025-02-23 14:04:17 +0700

    style(ui): change timeline

--- src/components/dashboard/timeline.tsx
+++ src/components/dashboard/timeline.tsx
@@ -2,107 +2,151 @@ import React from 'react';
 
 interface TimelineEvent {
   date: string;
-  description: string; // Add transform property
+  description: string;
 }
+
 interface TimelineProps {
   showCircle?: boolean;
+  showTurn?: boolean;
   month?: string;
   endingMonthState?: boolean;
   endingMonth?: string;
   events: TimelineEvent[];
 }
 
-
-const Timeline: React.FC<TimelineProps> = ({ showCircle = true, month, endingMonthState = false, endingMonth, events}) => {
-  const transform = [
-    'translateX(-20%)',
-    'translateX(10%)',
-    'translateX(-10%)',
-    'translateX(15%)',
-    'translateX(-20%)',
-    'translateX(10%)',
-    'translateX(-15%)',
-    'translateX(15%)',
-  ];
-
-  const bacgkroundImages = [
-    'url(/abstract/abstract1.svg)',
-    'url(/abstract/abstract2.svg)',
-    'url(/abstract/abstract3.svg)',
-    'url(/abstract/abstract4.svg)',
-    'url(/abstract/abstract5.svg)',
-  ];
-
-  const textColor = [
-    'text-[#5A8AF9]',
-    'text-[#FA3A91]',
-    'text-[#BEEF62]',
-    'text-[#FA3A91]',
-    'text-[#BEEF62]',
-  ];
-  
-  const getTransform = (index: number) => {
-    return transform[index % transform.length];
-  };
-
-  const getBackgroundImage = (index: number) => {
-    return bacgkroundImages[index % bacgkroundImages.length];
-  };
-
-  const getTextColor = (index: number) => {
-    return textColor[index % textColor.length];
-  };
-
+const Timeline: React.FC<TimelineProps> = ({
+  showCircle = true,
+  showTurn = false,
+  month,
+  endingMonthState = false,
+  endingMonth,
+  events
+}) => {
   return (
-    <div className="relative w-auto sm:w-full py-4 my-2"> {/* Reduce padding at the bottom */}
-      {/* Middle white line with dots */}
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-1 sm:w-2 h-[105%] bg-white my-[4px]">
-        <div className="absolute -top-6  left-1/2 transform -translate-x-1/2 w-8 h-8 border-8 border-white rounded-full"></div>
-        {showCircle && (
-          <div className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 w-8 h-8 border-8 border-white rounded-full"></div>
-        )}
-      </div>
-
-      {/* Starting month */}
-      <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mt-12 px-4 py-2 text-white text-[22px] md:text-[42px] font-bold rounded-full">
-        {month}
-      </div>
-
-      {/* Timeline events */}
-      <div className="">
-        {events.map((event, index) => (
-          <div
-            key={index}
-            className={`flex justify-between items-center w-full -mb-[50px] sm:-mb-[100px] lg:-mb-[160px] ${
-              index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'
-            }`} // Apply negative margin for overlap
-          >
-            {/* Date and description box */}
-            <div 
-              className={`text-white flex items-center justify-center w-[200px] h-[200px] sm:w-[320px] sm:h-[320px] md:w-[420px] md:h-[420px] lg:w-[540px] lg:h-[540px] xl:w-[680px] xl:h-[680px]`}
-              style={{ 
-                backgroundImage: getBackgroundImage(index), 
-                backgroundSize: 'contain',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center', 
-                transform: getTransform(index) // Use transform from event data
-              }}
-            >
-              <div className='flex flex-col w-full max-w-[140px] md:max-w-[240px]'>
-                <h3 className={`font-bold text-[9px] sm:text-[12px] md:text-[16px] lg:text-[20px] xl:text-[24px] ${getTextColor(index)}`}>{event.date}</h3>
-                <p className='text-white leading-[1] font-bold text-[12px] sm:text-[18px] md:text-[24px] lg:text-[32px] xl:text-[36px] md:text-shadow-md text-shadow-[0.5px_1px_1px_rgba(0,0,0,0.2)]'>{event.description}</p>
+    <div className="relative w-full min-h-[100px] px-8">
+      {/* Desktop: 2 rows */}
+      <div className="hidden md:block">
+        {/* First row */}
+        <div className="relative px-8">
+          <div className="absolute left-10 right-5 top-5 h-1 bg-[#e9e84f]" />
+          <div className="relative pt-4">
+            {events.slice(0, 5).map((event, index) => (
+              <div key={index} className="absolute" style={{ left: `${(index * 100) / (Math.min(events.length, 5) - 1)}%` }}>
+                 <div className="w-4 h-4 border-4 border-[#e9e84f] rounded-full bg-[#e9e84f]" />
+                 <div className="absolute mt-[-22px] left-[-7px] w-7 h-7 border-4 border-[#e9e84f] rounded-full bg-transparent" />
+                <div className="absolute mt-4 -translate-x-1/2 min-w-[120px]">
+                  <p className="text-[#FA3A91] text-sm font-bold">{event.date}</p>
+                  <p className="text-white text-sm font-bold mt-1">{event.description}</p>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
+        </div>
+
+        <div className="mt-1 absolute w-[62.5px] h-[125px] border-r-4 border-[#e9e84f] rounded-r-full bg-transparent right-[-10px] overflow-hidden">
+          <div className="w-[125px] h-[125px] border-4 border-[#e9e84f] rounded-full bg-transparent translate-x-[-50%]" />
+        </div>
+
+        {/* Second row */}
+        <div className="relative px-8">
+          <div className="absolute left-10 right-5 mt-[25px] h-1 bg-[#e9e84f]" />
+          <div className="relative pt-4 mt-[100px]">
+            {events.slice(5, 10).reverse().map((event, index) => (
+              <div key={index} className="absolute" style={{ left: `${(index * 100) / (Math.min(events.length, 5) - 1)}%` }}>
+                <div className="w-4 h-4 border-4 border-[#e9e84f] rounded-full bg-[#e9e84f]" />
+                <div className="absolute mt-[-22px] left-[-7px] w-7 h-7 border-4 border-[#e9e84f] rounded-full bg-transparent" />
+                <div className="absolute mt-4 -translate-x-1/2 min-w-[120px]">
+                  <p className="text-[#FA3A91] text-sm font-bold">{event.date}</p>
+                  <p className="text-white text-sm font-bold mt-1">{event.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
-      {/* Ending month */}
-      {endingMonthState && (
-        <div className="absolute mt-[15vw] left-1/2 transform -translate-x-1/2 text-white text-[22px] md:text-[42px] font-bold rounded-full">
-          {endingMonth}
+      {/* Mobile: 3 rows */}
+      <div className="block md:hidden">
+        {/* First row */}
+        <div className="relative ">
+          <div className="absolute left-3 right-[-3px] top-5 h-1 bg-[#e9e84f]" />
+          <div className="relative pt-4">
+            {events.slice(0, 4).map((event, index) => (
+              <div key={index} className="absolute" style={{ left: `${(index * 100) / 3}%` }}>
+                <div className="w-4 h-4 border-4 border-[#e9e84f] rounded-full bg-[#e9e84f]" />
+                <div className="absolute mt-[-22px] left-[-7px] w-7 h-7 border-4 border-[#e9e84f] rounded-full bg-transparent" />
+
+                <div className="absolute mt-4 -translate-x-1/2 min-w-[120px]">
+                  <p className="text-[#FA3A91] text-sm font-bold">{event.date}</p>
+                  <p className="text-white text-sm font-bold mt-1">{event.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      )}
+
+        <div className="mt-1 absolute w-[62.5px] h-[125px] border-r-4 border-[#e9e84f] rounded-r-full bg-transparent right-[-45px] overflow-hidden">
+          <div className="w-[125px] h-[125px] border-4 border-[#e9e84f] rounded-full bg-transparent translate-x-[-50%]" />
+        </div>
+
+        {/* Second row */}
+        <div className="relative">
+          <div className="absolute left-3 right-[-3px] mt-[22px] h-1 bg-[#e9e84f]" />
+          <div className="relative pt-4 mt-[100px]">
+            {events.slice(4, 7).map((event, index) => (
+              <div key={index} className="absolute" style={{ left: `${(index * 100) / 2}%` }}>
+                <div className="w-4 h-4 border-4 border-[#e9e84f] rounded-full bg-[#e9e84f]" />
+                <div className="absolute mt-[-22px] left-[-7px] w-7 h-7 border-4 border-[#e9e84f] rounded-full bg-transparent" />
+                <div className="absolute mt-4 -translate-x-1/2 min-w-[120px]">
+                  <p className="text-[#FA3A91] text-sm font-bold">{event.date}</p>
+                  <p className="text-white text-sm font-bold mt-1">{event.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-1 absolute w-[62.5px] h-[125px] bg-transparent left-[-28px] overflow-hidden">
+          <div className="w-[125px] h-[125px] border-4 border-[#e9e84f] rounded-full bg-transparent" />
+        </div>
+
+
+        {/* Third row */}
+        <div className="relative">
+          <div className="absolute left-3 right-[-3px] mt-[23px] h-1 bg-[#e9e84f]" />
+          <div className="relative pt-4 mt-[100px]">
+            {events.slice(7, 9).map((event, index) => (
+              <div key={index} className="absolute" style={{ left: `${(index * 100) / 1}%` }}>
+                <div className="w-4 h-4 border-4 border-[#e9e84f] rounded-full bg-[#e9e84f]" />
+                <div className="absolute mt-[-22px] left-[-7px] w-7 h-7 border-4 border-[#e9e84f] rounded-full bg-transparent" />
+                <div className="absolute mt-4 -translate-x-1/2 min-w-[120px]">
+                  <p className="text-[#FA3A91] text-sm font-bold">{event.date}</p>
+                  <p className="text-white text-sm font-bold mt-1">{event.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-1 absolute w-[62.5px] h-[125px] border-r-4 border-[#e9e84f] rounded-r-full bg-transparent right-[-45px] overflow-hidden">
+          <div className="w-[125px] h-[125px] border-4 border-[#e9e84f] rounded-full bg-transparent translate-x-[-50%]" />
+        </div>
+
+        <div className="relative">
+          <div className="relative pt-4 mt-[100px]">
+            {events.slice(9, 10).reverse().map((event, index) => (
+              <div key={index} className="absolute right-[-15px]">
+                <div className="w-4 h-4 border-4 border-[#e9e84f] rounded-full bg-[#e9e84f]" />
+                <div className="absolute mt-[-22px] left-[-7px] w-7 h-7 border-4 border-[#e9e84f] rounded-full bg-transparent" />
+                <div className="absolute mt-4 -translate-x-1/2 min-w-[120px]">
+                  <p className="text-[#FA3A91] text-sm font-bold">{event.date}</p>
+                  <p className="text-white text-sm font-bold mt-1">{event.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
