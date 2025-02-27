4bdff1b48e352c6ae4f623395873541093c622d9
Incheon21 <alvinchrisantausa@gmail.com>
2025-02-27 23:09:10 +0700

    fix: fix auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,24 +1,28 @@
 import NextAuth from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
 
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://pemirakmitb.com"
+    : "http://localhost:3000";
+
 export default NextAuth({
   providers: [
     AzureADProvider({
       clientId: process.env.MICROSOFT_CLIENT_ID ?? "",
       clientSecret: process.env.MICROSOFT_CLIENT_SECRET ?? "",
-      tenantId: process.env.MICROSOFT_TENANT_ID, // Optional: Specify your tenant ID
+      tenantId: process.env.MICROSOFT_TENANT_ID,
       authorization: {
         params: {
-          scope: "openid profile email", // Required scopes
+          scope: "openid profile email",
         },
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET, // Ensure this is set in your environment
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async signIn({ user, account, profile }) {
-      // Optional: Add any additional sign-in logic here
-      return true;
+    async redirect({ url, baseUrl: defaultBaseUrl }) {
+      return url.startsWith("/") ? `${baseUrl}${url}` : url;
     },
   },
 });
