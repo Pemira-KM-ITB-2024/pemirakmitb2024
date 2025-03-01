47631e2f95ae967540c70c7daa070dae6baffc8e
Incheon21 <alvinchrisantausa@gmail.com>
2025-03-01 13:36:00 +0700

    comment guide-vowing

--- src/pages/guide-voting.tsx
+++ src/pages/guide-voting.tsx
@@ -1,19 +1,19 @@
-import GuideVote from "~/components/voting";
-import Layout from '~/components/layout';
-import Bg from "~/components/background";
-import FaQ from "~/components/faq";
-import dynamic from "next/dynamic";
+// import GuideVote from "~/components/voting";
+// import Layout from '~/components/layout';
+// import Bg from "~/components/background";
+// import FaQ from "~/components/faq";
+// import dynamic from "next/dynamic";
 
-const DynamicMapComponent = dynamic(() => import('@/lokasi-voting'), { ssr: false });
+// const DynamicMapComponent = dynamic(() => import('@/lokasi-voting'), { ssr: false });
 
-const GuideVoting = () => {
-  return (
-    <div className="flex flex-col items-center justify-center gap-20 py-10 md:py-16">
-      <GuideVote />
-      <DynamicMapComponent />
-      <FaQ />
-    </div>
-  );
-};
+// const GuideVoting = () => {
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-20 py-10 md:py-16">
+//       <GuideVote />
+//       <DynamicMapComponent />
+//       <FaQ />
+//     </div>
+//   );
+// };
 
-export default GuideVoting;
+// export default GuideVoting;
