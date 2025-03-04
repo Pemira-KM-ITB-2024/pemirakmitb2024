7bcdb2ed471a41c5f31148db5c9b56cb40af64ce
Incheon21 <alvinchrisantausa@gmail.com>
2025-03-04 21:53:10 +0700

    remove session file

--- src/pages/api/auth/session.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { getSession } from "next-auth/react";
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-  res.status(200).json(session);
-};
-
-export default handler;
