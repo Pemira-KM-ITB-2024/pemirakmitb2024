d343b2009a8fc8311175d3816fd73e62dfa1f578
Incheon21 <alvinchrisantausa@gmail.com>
2025-03-05 16:28:26 +0700

    fix signout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -5,15 +5,18 @@ const signOutHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const token = await getToken({ req });
 
   const isProduction = process.env.NODE_ENV === "production";
-  const domain = isProduction ? "pemirakmitb.com" : "localhost";
   const secure = isProduction ? "Secure;" : "";
 
   if (token) {
     res.setHeader("Set-Cookie", [
-      `__Secure-next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; ${secure} SameSite=Lax; Domain=${domain}`,
-      `__Host-next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; ${secure} SameSite=Lax; Domain=${domain}`,
-      `next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax; Domain=${domain}`,
-      `next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax; Domain=${domain}`,
+      // Production cookies
+      `__Secure-next-auth.session-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; ${secure} SameSite=Lax`,
+      `__Host-next-auth.csrf-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; ${secure} SameSite=Lax`,
+      // Development cookies
+      `next-auth.session-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Lax`,
+      `next-auth.csrf-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Lax`,
+      // Callback URL cookie
+      `next-auth.callback-url=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; ${secure} SameSite=Lax`,
     ]);
     res.status(200).json({ message: "Signed out successfully" });
   } else {
