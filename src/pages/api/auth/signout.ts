035c0842522cea0f4bf6e2cb367be56d9cdf5363
Incheon21 <alvinchrisantausa@gmail.com>
2025-03-05 16:14:50 +0700

    fix sign out

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -4,12 +4,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const signOutHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const token = await getToken({ req });
 
+  const isProduction = process.env.NODE_ENV === "production";
+  const domain = isProduction ? "pemirakmitb.com" : "localhost";
+  const secure = isProduction ? "Secure;" : "";
+
   if (token) {
     res.setHeader("Set-Cookie", [
-      `__Secure-next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax`,
-      `__Host-next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax`,
-      `next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax`,
-      `next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax`,
+      `__Secure-next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; ${secure} SameSite=Lax; Domain=${domain}`,
+      `__Host-next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; ${secure} SameSite=Lax; Domain=${domain}`,
+      `next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax; Domain=${domain}`,
+      `next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax; Domain=${domain}`,
     ]);
     res.status(200).json({ message: "Signed out successfully" });
   } else {
