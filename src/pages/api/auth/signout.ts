14febe79f9eb81487683958afe76c72f012c03da
Incheon21 <alvinchrisantausa@gmail.com>
2025-03-05 16:02:08 +0700

    fix signout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -5,12 +5,11 @@ const signOutHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const token = await getToken({ req });
 
   if (token) {
-    // Clear the session token
     res.setHeader("Set-Cookie", [
-      `next-auth.session-token=; Path=/; Max-Age=0; HttpOnly`,
-      `__Secure-next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; Secure`,
-      `next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly`,
-      `__Secure-next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; Secure`,
+      `__Secure-next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax`,
+      `__Host-next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax`,
+      `next-auth.session-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax`,
+      `next-auth.csrf-token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax`,
     ]);
     res.status(200).json({ message: "Signed out successfully" });
   } else {
